9ea1e5b7d2af1b79253fb03592d0f775bee1ec64
minu <dnalschl1989@gmail.com>
2026-02-09 23:03:07 +0900

    Update and rename 01.Filtering_Selection to 01.Filtering_Selection.py

--- python/preprocessing/01.Filtering_Selection.py
+++ python/preprocessing/01.Filtering_Selection.py
@@ -1,8 +1,8 @@
-/*quantity == 3인 데이터의 첫 5행*/
+#quantity == 3인 데이터의 첫 5행
 Ans = df.loc[df["quantity"] == 3].head(5)
 
-/*item_name에 'Chips' 포함하는 행*/
+#item_name에 'Chips' 포함하는 행
 Ans = df.loc[df["item_name"].str.contains("Chips")]
 
-/*item_name에 'Chips' 포함하지 않는 행*/
+#item_name에 'Chips' 포함하지 않는 행
 df.loc[~df["item_name"].str.contains("Chips")]
